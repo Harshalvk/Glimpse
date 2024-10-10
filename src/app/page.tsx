b81eb71e1342a9168191e3c482e434b441a972cd
Harshalvk <harshalvkhobragade@gmail.com>
2024-10-10 23:46:18 +0530

    ADD: Pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import Hero from "@/components/Hero";
 import Header from "../components/Header";
 import LogoTicker from "@/components/LogoTicker";
 import ProductShowcase from "@/components/ProductShowcase";
+import Pricing from "@/components/Pricing";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <LogoTicker/>
-      <ProductShowcase/>
+      <LogoTicker />
+      <ProductShowcase />
+      <Pricing />
     </>
   );
 }
